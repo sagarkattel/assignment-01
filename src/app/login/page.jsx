828c5080ecf386014940d8de1b66e2bc267689fb
sagarkattel <71958289+sagarkattel@users.noreply.github.com>
2023-09-18 17:24:02 +0530

    loe

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -1,5 +1,6 @@
+'use client';
 import React from 'react';
-
+import Link from 'next/link';
 import { AiFillGithub, AiFillTwitterCircle, AiFillLinkedin } from 'react-icons/ai';
 
 import { BiLogoDiscord } from 'react-icons/bi';
@@ -90,13 +91,15 @@ const Login = () => {
             <div className="text-blue-600 mb-5 text-xs">
               <a href="#">Forget Password?</a>
             </div>
-
+            <Link href="/dashboard">
             <button
               type="button"
               className="text-white bg-[#3e81f1] hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
+              
             >
-              Sign In
+            Sign In
             </button>
+            </Link>
           </form>
           <div className="pl-[25px] text-gray-500 text-xs">
             Dont have an account?{' '}
